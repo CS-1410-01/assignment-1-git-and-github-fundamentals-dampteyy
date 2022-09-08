6a53f86b7c5a3b7db53fd22ee5492c5c483092db
dampteyy <113066732+dampteyy@users.noreply.github.com>
2022-09-07 18:26:35 -0600

    Update Hello.java
    finished

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -1,10 +1,10 @@
-
+package com.example.project;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello");
+		System.out.println("Hello world1");
 	}
 
 }
